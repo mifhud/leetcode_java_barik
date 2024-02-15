2055ac07421a467e5662e2f623bc6d97a8eddabe
Miftahul Huda <miftahul.huda@jatis.com>
2024-02-15 10:56:29 +0700

    feat(fizz_buzz): fizz buzz

---
+++ 02_integers/fizz_buzz/src/main/java/org/example/App.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+    public static List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                res.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                res.add("Fizz");
+            } else if (i % 5 == 0) {
+                res.add("Buzz");
+            } else {
+                res.add(Integer.toString(i));
+            }
+        }
+        return res;
+    }
+}
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = bufferedReader.readLine();
+
+        List<String> result = Solution.fizzBuzz(Integer.parseInt(s));
+
+        System.out.println(result);
+
+        bufferedReader.close();
+    }
+}
