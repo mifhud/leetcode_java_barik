aae5f769771f55fe487ad5b95032b15afe9610e7
Miftahul Huda <miftahul.huda@jatis.com>
2024-02-15 21:07:32 +0700

    feat(single_number): single number

---
+++ 02_integers/single_number/src/main/java/org/example/App.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+
+class Solution {
+
+    public static int singleNumber(int[] nums) {
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+}
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .mapToInt(i -> i)
+                .toArray();
+
+        int result = Solution.singleNumber(ar);
+
+        System.out.println(result);
+
+        bufferedReader.close();
+    }
+}
