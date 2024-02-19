beecdf75b8b4d89ba362c7ddb8aefbcebdbff268
Miftahul Huda <miftahul.huda@jatis.com>
2024-02-19 15:10:02 +0700

    feat(reverse_integer): reverse_integer

---
+++ 02_integers/reverse_integer/src/main/java/org/example/App.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+
+    public static int reverse(int x) {
+        int reversed = 0;
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+        while (x != 0) {
+            int lastDigit = x % 10;
+
+            if (reversed > max / 10 || (reversed == max / 10 && lastDigit > 7)) {
+                return 0;
+            }
+
+            if (reversed < min / 10 || (reversed == min / 10 && lastDigit < -8)) {
+                return 0;
+            }
+
+            reversed = reversed * 10 + lastDigit;
+
+            x = x / 10;
+        }
+
+        return reversed;
+    }
+}
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String num = bufferedReader.readLine();
+
+        int result = Solution.reverse(Integer.parseInt(num));
+
+        System.out.println(result);
+
+        bufferedReader.close();
+    }
+}
